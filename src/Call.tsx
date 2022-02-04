72285c7cbe92ff35920c5b99a08658fa1c6e3615
Nikola_Mandic <nikola.mandic@liveweb.io>
2022-02-04 16:30:48 +0100

    multiple fixes

--- src/Call.tsx
+++ src/Call.tsx
@@ -1,81 +1,136 @@
-import React, { useState, useRef } from 'react';
-import AgoraRTC, { IAgoraRTCClient } from 'agora-rtc-sdk-ng';
-import useAgora from './hooks/useAgora';
-import MediaPlayer from './components/MediaPlayer';
-import JoinForm, { JoinData } from './components/JoinForm';
-import './Call.css';
+import React, { useState, useRef } from "react";
+import AgoraRTC, { IAgoraRTCClient } from "agora-rtc-sdk-ng";
+import useAgora from "./hooks/useAgora";
+import MediaPlayer from "./components/MediaPlayer";
+import JoinForm, { JoinData } from "./components/JoinForm";
+import "./Call.css";
 
 function Call() {
+  const [clients, setClients] = useState<IAgoraRTCClient[]>([]);
 
-  const [ clients, setClients ] = useState<IAgoraRTCClient[]>([])
-
-  const [ publishing, setPublishing ] = useState<boolean>(true)
+  const [publishing, setPublishing] = useState<boolean>(true);
 
   const {
-    localAudioTrack, localVideoTrack, leave, join, remoteUsers, volumeIndicator, muteVideo, muteAudio, muteVideoState, muteAudioState, publishingClient
+    localAudioTrack,
+    localVideoTrack,
+    leave,
+    join,
+    remoteUsers,
+    volumeIndicator,
+    muteVideo,
+    muteAudio,
+    muteVideoState,
+    muteAudioState,
+    publishingClient
   } = useAgora();
 
-  const addFormButton = useRef<HTMLButtonElement>(null)
-
-  // const sendInitialData = () => {
-  //   const sessionAppID = window.localStorage.getItem('Agora_Sample_appid');
-  //   const sessionToken = window.localStorage.getItem('Agora_Sample_token');
-  //   const sessionChannel = window.localStorage.getItem('Agora_Sample_channel');
-  //   return {appid: sessionAppID, token: sessionToken, channel: sessionChannel}
-  // }
+  const addFormButton = useRef<HTMLButtonElement>(null);
 
   const handleAddJoinForm = () => {
-    const newChannel = AgoraRTC.createClient({ codec: 'h264', mode: 'live'});
-    clients.length ? setClients([ ...clients, newChannel]) : setClients([newChannel])
-  }
+    const newChannel = AgoraRTC.createClient({ codec: "h264", mode: "live" });
+    clients.length
+      ? setClients([...clients, newChannel])
+      : setClients([newChannel]);
+  };
 
   const handleJoinRequest = async (data: JoinData) => {
-    if(data.appid && data.token && data.channel){
-      join(data.client, data.appid, data.channel, publishing, data.token)
-      setPublishing(false)
-    }
-    else{
+    if (data.appid && data.channel) {
+      setPublishing(true);
+      join(
+        data.client,
+        data.appid,
+        data.channel,
+        publishing,
+        data.token ? data.token : null
+      );
+    } else {
       console.error("Can't join client, missing data!");
     }
-  }
+  };
 
   const handleLeaveRequest = async (data: IAgoraRTCClient) => {
-    if(data){
-      const filterClients = clients.filter(client => client !== data)
-      setClients(filterClients)
-      filterClients.length ? leave(data) : leave(data, true)
-
+    if (data) {
+      const filterClients = clients.filter((client) => client !== data);
+      setClients(filterClients);
+      filterClients.length ? leave(data) : leave(data, true);
     }
-  }
+  };
   return (
-    <div className='call'>
-      <button ref={addFormButton} className="aditional-form-button" onClick={handleAddJoinForm}><i className="fas fa-plus-circle"></i></button>
-      {
-        clients.map((cl, index) => {
-          return <JoinForm returnData={handleJoinRequest} returnLeave={handleLeaveRequest} client={cl} key={index}></JoinForm>
-        })
-      }
+    <div className="call">
+      <button
+        ref={addFormButton}
+        className="aditional-form-button"
+        onClick={handleAddJoinForm}
+      >
+        <i className="fas fa-plus-circle"></i>
+      </button>
+      {clients.map((cl, index) => {
+        return (
+          <JoinForm
+            returnData={handleJoinRequest}
+            returnLeave={handleLeaveRequest}
+            client={cl}
+            key={index}
+          ></JoinForm>
+        );
+      })}
 
-      <div className='player-container'>
-        <div className='local-player-wrapper'>
-          <p className='local-player-text'>{localVideoTrack && `localTrack`}{localVideoTrack ? `(${publishingClient?.uid})` : ''}</p>
-          <MediaPlayer videoTrack={localVideoTrack} audioTrack={localAudioTrack} uid={publishingClient?.uid} volumeIndicator={volumeIndicator} ></MediaPlayer>
-          {localVideoTrack ?
+      <div className="player-container">
+        <div className="local-player-wrapper">
+          <p className="local-player-text">
+            {localVideoTrack && `localTrack`}
+            {localVideoTrack ? `(${publishingClient?.uid})` : ""}
+          </p>
+          <MediaPlayer
+            videoTrack={localVideoTrack}
+            audioTrack={localAudioTrack}
+            uid={publishingClient?.uid}
+            volumeIndicator={volumeIndicator}
+          ></MediaPlayer>
+          {localVideoTrack ? (
             <div className="controll-buttons">
-              <div className="mute-video" onClick={()=>{muteVideo()}}><i 
-                className={ muteVideoState ? "fas fa-video" : "fas fa-video-slash"}></i>
+              <div
+                className="mute-video"
+                onClick={() => {
+                  muteVideo();
+                }}
+              >
+                <i
+                  className={
+                    muteVideoState ? "fas fa-video" : "fas fa-video-slash"
+                  }
+                ></i>
               </div>
-              <div className="mute-audio" onClick={()=>{muteAudio()}}><i 
-                className={ muteAudioState ? "fas fa-microphone" : "fas fa-microphone-slash"}></i>
+              <div
+                className="mute-audio"
+                onClick={() => {
+                  muteAudio();
+                }}
+              >
+                <i
+                  className={
+                    muteAudioState
+                      ? "fas fa-microphone"
+                      : "fas fa-microphone-slash"
+                  }
+                ></i>
               </div>
             </div>
-            : ''
-          }
+          ) : (
+            ""
+          )}
         </div>
-        {remoteUsers.map(user => (<div className='remote-player-wrapper' key={user.uid}>
-            <p className='remote-player-text'>{`remoteVideo(${user.uid})`}</p>
-            <MediaPlayer videoTrack={user.videoTrack} audioTrack={user.audioTrack} uid={user.uid} volumeIndicator={volumeIndicator}></MediaPlayer>
-          </div>))}
+        {remoteUsers.map((user) => (
+          <div className="remote-player-wrapper" key={user.uid}>
+            <p className="remote-player-text">{`remoteVideo(${user.uid})`}</p>
+            <MediaPlayer
+              videoTrack={user.videoTrack}
+              audioTrack={user.audioTrack}
+              uid={user.uid}
+              volumeIndicator={volumeIndicator}
+            ></MediaPlayer>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/hooks/useAgora.tsx
+++ src/hooks/useAgora.tsx
@@ -1,69 +1,105 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import AgoraRTC, {
-  IAgoraRTCClient, IAgoraRTCRemoteUser, MicrophoneAudioTrackInitConfig, CameraVideoTrackInitConfig, IMicrophoneAudioTrack, ICameraVideoTrack, ILocalVideoTrack, ILocalAudioTrack, ILocalTrack } from 'agora-rtc-sdk-ng';
+  IAgoraRTCClient,
+  IAgoraRTCRemoteUser,
+  MicrophoneAudioTrackInitConfig,
+  CameraVideoTrackInitConfig,
+  IMicrophoneAudioTrack,
+  ICameraVideoTrack,
+  ILocalVideoTrack,
+  ILocalAudioTrack,
+  ILocalTrack
+} from "agora-rtc-sdk-ng";
 
 export interface volume {
-    uid: number,
-    level: number    
+  uid: number;
+  level: number;
 }
-export default function useAgora()
-  :
-   {
-      localAudioTrack: ILocalAudioTrack | undefined,
-      localVideoTrack: ILocalVideoTrack | undefined,
-      leave: Function,
-      join: Function,
-      remoteUsers: IAgoraRTCRemoteUser[],
-      volumeIndicator: Array<volume> | undefined,
-      muteVideo: Function;
-      muteVideoState: boolean;
-      muteAudio: Function;
-      muteAudioState: boolean;
-      publishingClient: IAgoraRTCClient | undefined
-    }
-    {
-  const [clients, setClients] = useState<IAgoraRTCClient[] | undefined>([])
-  const [ publishingClient, setPublishingClient ] = useState<IAgoraRTCClient | undefined>(undefined)
-  const [localVideoTrack, setLocalVideoTrack] = useState<ILocalVideoTrack | undefined>(undefined);
-  const [localAudioTrack, setLocalAudioTrack] = useState<ILocalAudioTrack | undefined>(undefined);
-  const [volumeIndicator, setVolumeIndicator] = useState<Array<volume> | undefined>(undefined)
-  const [muteVideoState, setMuteVideoState] = useState(false)
-  const [muteAudioState, setMuteAudioState] = useState(false)
+export default function useAgora(): {
+  localAudioTrack: ILocalAudioTrack | undefined;
+  localVideoTrack: ILocalVideoTrack | undefined;
+  leave: Function;
+  join: Function;
+  remoteUsers: IAgoraRTCRemoteUser[];
+  volumeIndicator: Array<volume> | undefined;
+  muteVideo: Function;
+  muteVideoState: boolean;
+  muteAudio: Function;
+  muteAudioState: boolean;
+  publishingClient: IAgoraRTCClient | undefined;
+} {
+  const [clients, setClients] = useState<IAgoraRTCClient[] | undefined>([]);
+  const [publishingClient, setPublishingClient] = useState<
+    IAgoraRTCClient | undefined
+  >(undefined);
+  const [localVideoTrack, setLocalVideoTrack] = useState<
+    ILocalVideoTrack | undefined
+  >(undefined);
+  const [localAudioTrack, setLocalAudioTrack] = useState<
+    ILocalAudioTrack | undefined
+  >(undefined);
+  const [volumeIndicator, setVolumeIndicator] = useState<
+    Array<volume> | undefined
+  >(undefined);
+  const [muteVideoState, setMuteVideoState] = useState(false);
+  const [muteAudioState, setMuteAudioState] = useState(false);
 
   const [remoteUsers, setRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([]);
 
-  async function createLocalTracks(audioConfig?: MicrophoneAudioTrackInitConfig, videoConfig?: CameraVideoTrackInitConfig)
-  : Promise<[IMicrophoneAudioTrack, ICameraVideoTrack]> {
-    const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks(audioConfig, videoConfig);
+  async function createLocalTracks(
+    audioConfig?: MicrophoneAudioTrackInitConfig,
+    videoConfig?: CameraVideoTrackInitConfig
+  ): Promise<[IMicrophoneAudioTrack, ICameraVideoTrack]> {
+    const [
+      microphoneTrack,
+      cameraTrack
+    ] = await AgoraRTC.createMicrophoneAndCameraTracks(
+      audioConfig,
+      videoConfig
+    );
     setLocalAudioTrack(microphoneTrack);
     setLocalVideoTrack(cameraTrack);
     return [microphoneTrack, cameraTrack];
   }
 
-  const publishClient = async (publishingClient: IAgoraRTCClient,tracks: ILocalTrack[]) => {
+  const publishClient = async (
+    publishingClient: IAgoraRTCClient,
+    tracks: ILocalTrack[]
+  ) => {
     console.log("Publishing tracks...");
-    await publishingClient.setClientRole('host');
+    await publishingClient.setClientRole("host");
     await publishingClient.publish(tracks);
-    setPublishingClient(publishingClient)
-  }
+    setPublishingClient(publishingClient);
+  };
 
-  async function join(newClient: IAgoraRTCClient, appid: string, channel: string, publish: boolean, token?: string, uid?: string | number | null) {
+  async function join(
+    newClient: IAgoraRTCClient,
+    appid: string,
+    channel: string,
+    publish: boolean,
+    token?: string,
+    uid?: string | number | null
+  ) {
     if (!newClient) return;
-    clients?.length ? await setClients([...clients, newClient]) : await setClients([newClient])
-    console.log(`Joining: \nChannel: ${channel} \nClient: `, newClient, `\nAPP ID: ${appid} \nToken: ${token}`)
-    console.log('CLIENTS!!!', publish, uid)
+    clients?.length
+      ? await setClients([...clients, newClient])
+      : await setClients([newClient]);
+    console.log(
+      `Joining: \nChannel: ${channel} \nClient: `,
+      newClient,
+      `\nAPP ID: ${appid} \nToken: ${token}`
+    );
     let microphoneTrack, cameraTrack;
-    if(publish){
+    if (publish) {
       [microphoneTrack, cameraTrack] = await createLocalTracks();
     }
-    
+
     await newClient.join(appid, channel, token || null);
-    if(publish && microphoneTrack && cameraTrack){
-      publishClient(newClient, [microphoneTrack, cameraTrack])
-    }
-    else {
+    if (publish && microphoneTrack && cameraTrack) {
+      publishClient(newClient, [microphoneTrack, cameraTrack]);
+    } else {
       console.log("Skipping publishing tracks...");
-      await newClient.setClientRole('audience')
+      await newClient.setClientRole("audience");
     }
     await newClient.enableAudioVolumeIndicator();
 
@@ -72,7 +108,7 @@ export default function useAgora()
   }
 
   async function leave(client: IAgoraRTCClient, lastClient: boolean) {
-    if(lastClient){
+    if (lastClient) {
       if (localAudioTrack) {
         localAudioTrack.stop();
         localAudioTrack.close();
@@ -80,61 +116,64 @@ export default function useAgora()
       if (localVideoTrack) {
         localVideoTrack.stop();
         localVideoTrack.close();
-        setLocalVideoTrack(undefined)
+        setLocalVideoTrack(undefined);
       }
       setRemoteUsers([]);
-      setVolumeIndicator(undefined)  
+      setVolumeIndicator(undefined);
     }
-    console.log('Leaving client:', client)
+    console.log("Leaving client:", client);
+    setMuteVideoState(false);
+    setMuteAudioState(false);
     await client?.leave();
   }
 
   const muteVideo = async () => {
-    localVideoTrack?.setEnabled(muteVideoState)
-    setMuteVideoState(!muteVideoState)
-  }
+    localVideoTrack?.setEnabled(muteVideoState);
+    setMuteVideoState(!muteVideoState);
+  };
   const muteAudio = async () => {
-    console.log("MuteAudioState: ", muteAudioState)
-    localAudioTrack?.setEnabled(muteAudioState)
-    setMuteAudioState(!muteAudioState)
-  }
+    console.log("MuteAudioState: ", muteAudioState);
+    localAudioTrack?.setEnabled(muteAudioState);
+    setMuteAudioState(!muteAudioState);
+  };
 
   useEffect(() => {
     if (!clients) return;
-    clients.forEach(client => {
-      
+    clients.forEach((client) => {
       setRemoteUsers(client.remoteUsers);
 
-      const handleUserPublished = async (user: IAgoraRTCRemoteUser, mediaType: 'audio' | 'video') => {
+      const handleUserPublished = async (
+        user: IAgoraRTCRemoteUser,
+        mediaType: "audio" | "video"
+      ) => {
         await client.subscribe(user, mediaType);
-        if(mediaType === 'video')
-        console.log("User published", user)
+        if (mediaType === "video") console.log("User published", user);
         // toggle rerender while state of remoteUsers changed.
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
+        setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers));
+      };
       const handleUserUnpublished = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
+        setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers));
+      };
       const handleUserJoined = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
+        setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers));
+      };
       const handleUserLeft = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
-      const handleVolumeIndicator = (volumes: Array<volume> ) => {
-        setVolumeIndicator(volumes)
-      }
-      client.on('user-published', handleUserPublished);
-      client.on('user-unpublished', handleUserUnpublished);
-      client.on('user-joined', handleUserJoined);
-      client.on('user-left', handleUserLeft);
-      client.on('volume-indicator', handleVolumeIndicator)
+        setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers));
+      };
+      const handleVolumeIndicator = (volumes: Array<volume>) => {
+        setVolumeIndicator(volumes);
+      };
+      client.on("user-published", handleUserPublished);
+      client.on("user-unpublished", handleUserUnpublished);
+      client.on("user-joined", handleUserJoined);
+      client.on("user-left", handleUserLeft);
+      client.on("volume-indicator", handleVolumeIndicator);
 
       return () => {
-        client.off('user-published', handleUserPublished);
-        client.off('user-unpublished', handleUserUnpublished);
-        client.off('user-joined', handleUserJoined);
-        client.off('user-left', handleUserLeft);
+        client.off("user-published", handleUserPublished);
+        client.off("user-unpublished", handleUserUnpublished);
+        client.off("user-joined", handleUserJoined);
+        client.off("user-left", handleUserLeft);
       };
     });
   }, [clients]);
